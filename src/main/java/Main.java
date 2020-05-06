c33e560824f410ca257c98729a96e6d61c6d010b
haruna-ota <h-ota@biglobe.co.jp>
2020-05-06 14:46:59 +0900

    コマンドライン引数の順番固定をやめた

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,3 @@
-import com.sun.istack.internal.NotNull;
 import convert.Capitalize;
 import convert.Reverse;
 import print.Horizontal;
@@ -18,72 +17,30 @@ public class Main {
         List<String> convertOption = new ArrayList<>();
         //出力オプション
         String printOption = "";
+        //文字列
+        List<String> word = new ArrayList<>();
 
-        //まず引数の中から変換オプションを全て探す
+        //まず引数の中から変換オプション,出力オプション、文字を全て探す
         for (int i = 0; i < list.size(); i++) {
             if (list.get(i).equals("--capitalize") || list.get(i).equals("--reverse")) {
                 convertOption.add(list.get(i));
+            } else if (list.get(i).equals("--horizontal") || list.get(i).equals("--vertical")) {
+                printOption = list.get(i);
+            } else {
+                word = Stream.of(list.get(i).split(" ")).collect(Collectors.toList());
             }
         }
         System.out.println(convertOption);
-
-        //引数の中から出力オプションを探す
-        for (int i = 0; i < list.size(); i++) {
-            if (list.get(i).equals("--horizontal") || list.get(i).equals("--vertical")) {
-                printOption = list.get(i);
-            }
-        }
         System.out.println(printOption);
-
-        //引数の文字列に全ての変換を適用
-        List<String> word = Stream.of(list.get(0).split(" ")).collect(Collectors.toList());
         System.out.println("変換前：" + word);
 
+        //引数の文字列に全ての変換を適用
         for (int i = 0; i < convertOption.size(); i++) {
             word = convert(convertOption.get(i), word);
         }
         System.out.println("変換後：" + word);
 
         System.out.println("完成：" + print(printOption, word));
-
-//        String convertOption = null;
-//        String printOption = null;
-//        List<String> word = new ArrayList<>();
-//
-//        for (int i = 0; i < list.size(); i++) {
-//            if (list.get(i).equals("--horizontal") || list.get(i).equals("--vertical")) {
-//                printOption = list.get(i);
-//            } else if (list.get(i).equals("--capitalize") || list.get(i).equals("--reverse")) {
-//                convertOption = list.get(i);
-//            } else {
-//                word = Stream.of(list.get(i).split(" ")).collect(Collectors.toList());
-//            }
-//        }
-//
-//        System.out.println(word + " " + convertOption + " " + printOption);
-//        System.out.println(printWord(word, convertOption, printOption));
-//    }
-//
-//    public static String printWord(List<String> word, String convertOption, String printOption) {
-//        if (printOption.equals("--horizontal")) {
-//            if (convertOption.equals("--capitalize")) {
-//                return (Horizontal.print(Capitalize.convert(word)));
-//            } else if (convertOption.equals("--reverse")) {
-//                return (Horizontal.print(Reverse.convert(word)));
-//            } else {
-//                throw new RuntimeException("想定外の変換オプションです");
-//            }
-//        } else if (printOption.equals("--vertical")) {
-//            if (convertOption.equals("--capitalize")) {
-//                return (Vertical.print(Capitalize.convert(word)));
-//            } else if (convertOption.equals("--reverse")) {
-//                return (Vertical.print(Reverse.convert(word)));
-//            } else {
-//                throw new RuntimeException("想定外の変換オプションです");
-//            }
-//        } else {
-//            throw new RuntimeException("想定外の出力オプションです");
-//        }
     }
 
     public static List<String> convert(String convertOption, List<String> word) {
